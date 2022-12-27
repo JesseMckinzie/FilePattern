3cfa578e57ff7294cdebcb52e149141ff48137bc
JesseMckinzie <72471813+JesseMckinzie@users.noreply.github.com>
2022-12-27 11:53:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import filepattern2 as fp
 path = "/Users/jessemckinzie/Desktop/data/int"
 
-pattern = fp.infer_pattern(path=path, variables='', block_size='50 GB')
+#pattern = fp.infer_pattern(path=path, variables='', block_size='50 GB')
 
 
-#pat = fp.FilePattern(path, 'p{p:d}_y{y:d}_r{r:d}_c{c:d}.ome.tif', '50 GB')
+pat = fp.FilePattern(path, 'p{p:d}_y{y:d}_r{r:d}_c{c:d}.ome.tif')
 
-#for file in pat():
-#    print(file)
+for file in pat():
+    print(file)
     
